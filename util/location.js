5f82494fd87516fab313dba260feab38f2d7b2ae
ara fa adri <arafa.sco@gmail.com>
2025-07-13 23:49:35 +0700

    delete key

--- util/location.js
+++ util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyCNvcrpcgE1sYHeyklNr2Ao7Xn6R244b_U";
+const GOOGLE_API_KEY = "";
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=16&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
